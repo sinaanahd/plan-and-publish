56a5be9476c052d94f0fdf7f8456f81b228b6bc3
sinananhid <sina.anahid@outlook.com>
2024-07-31 16:57:20 +0330

    main responsive done / header hover effect added

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -11,6 +11,18 @@ import valued_icon_3 from "../../asset/images/valued-icon-3.svg";
 import valued_icon_4 from "../../asset/images/valued-icon-4.svg";
 import valued_icon_5 from "../../asset/images/valued-icon-5.svg";
 
+import valued_icon_1_gray from "../../asset/images/valued-icon-1-gray.svg";
+import valued_icon_2_gray from "../../asset/images/valued-icon-2-gray.svg";
+import valued_icon_3_gray from "../../asset/images/valued-icon-3-gray.svg";
+import valued_icon_4_gray from "../../asset/images/valued-icon-4-gray.svg";
+import valued_icon_5_gray from "../../asset/images/valued-icon-5-gray.svg";
+
+import valued_icon_1_blue from "../../asset/images/valued-icon-1-blue.svg";
+import valued_icon_2_blue from "../../asset/images/valued-icon-2-blue.svg";
+import valued_icon_3_blue from "../../asset/images/valued-icon-3-blue.svg";
+import valued_icon_4_blue from "../../asset/images/valued-icon-4-blue.svg";
+import valued_icon_5_blue from "../../asset/images/valued-icon-5-blue.svg";
+
 import middle_img from "../../asset/images/middle-img.svg";
 import left_1 from "../../asset/images/left-1.svg";
 import left_2 from "../../asset/images/left-2.svg";
@@ -26,45 +38,51 @@ import final_img from "../../asset/images/final-cta-img.svg";
 import video_hero from "../../asset/videos/hero-video.mp4";
 import scrollToTop from "../scroll";
 
+import valued_video_1 from "../../asset/videos/01_Drag_and_Drop.mp4";
+import valued_video_2 from "../../asset/videos/02_Customizable_App.mp4";
+import valued_video_3 from "../../asset/videos/03_Live_preview.mp4";
+import valued_video_4 from "../../asset/videos/04_Responsive_App.mp4";
+import valued_video_5 from "../../asset/videos/05_Publish.mp4";
+
 const Home = () => {
   const [valued_items, set_valued_items] = useState([
     {
       id: 1,
-      img: valued_icon_1,
+      img: [valued_icon_1, valued_icon_1_blue, valued_icon_1_gray],
       title: "Drag and Drop Interface",
       text: "No coding required, Drag and drop the modules you want to use in your app.",
-      content: "",
+      content: valued_video_1,
     },
     {
       id: 2,
-      img: valued_icon_2,
+      img: [valued_icon_2, valued_icon_2_blue, valued_icon_2_gray],
       title: "Customizable App",
       text: "Choose from a ton of templates like e-commerce, socials, education, etc.",
-      content: "",
+      content: valued_video_2,
     },
     {
       id: 3,
-      img: valued_icon_3,
+      img: [valued_icon_3, valued_icon_3_blue, valued_icon_3_gray],
       title: "Live Preview",
       text: "See how your app looks and work in real time as you build and improve it.",
-      content: "",
+      content: valued_video_3,
     },
     {
       id: 4,
-      img: valued_icon_4,
+      img: [valued_icon_4, valued_icon_4_blue, valued_icon_4_gray],
       title: "Responsive Design",
       text: "Your app will look great on any device, no matter the size and view port.",
-      content: "",
+      content: valued_video_4,
     },
     {
       id: 5,
-      img: valued_icon_5,
+      img: [valued_icon_5, valued_icon_5_blue, valued_icon_5_gray],
       title: "Plan and Publish",
       text: "Once your app is ready, you can publish it to the app stores around the world.",
-      content: "",
+      content: valued_video_5,
     },
   ]);
-  const [valued_contents, set_valued_contents] = useState(1);
+  const [valued_contents, set_valued_contents] = useState(3);
   return (
     <>
       <Helmet>
@@ -81,6 +99,10 @@ const Home = () => {
             On our way to unveil a groundbreaking platform
             <br />
             for app creation, pioneering the future of innovation.
+          </p>
+          <p className="hero-desc-mobile">
+            On our way to unveil a groundbreaking platform for app creation,
+            pioneering the future of innovation.
           </p>
           <button className="request-btn">Request early access</button>
           <div className="video-place">
@@ -120,7 +142,16 @@ const Home = () => {
                   set_valued_contents(vi.id);
                 }}
               >
-                <img src={vi.img} alt={vi.title} />
+                <img
+                  src={vi.img[0]}
+                  alt={vi.title}
+                  className="desktop-valued-icon"
+                />
+                <img
+                  src={vi.id === valued_contents ? vi.img[1] : vi.img[2]}
+                  alt={vi.title}
+                  className="responsive-valued-item"
+                />
                 <div className="option-texts">
                   <h3 className="option-title">{vi.title}</h3>
                   <p className="option-text">{vi.text}</p>
@@ -129,7 +160,14 @@ const Home = () => {
             ))}
           </div>
           <div className="valued-content-wrapper">
-            <img src={sample_valued} alt="" />
+            <video
+              src={valued_items[valued_contents - 1].content}
+              autoPlay
+              loop
+              type="video/mp4"
+              muted
+              className="video-hero"
+            ></video>
           </div>
         </div>
       </section>
